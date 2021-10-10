e62063d902cf8123369754811e91146189e83111
alurue9140 <91330447+alurue9140@users.noreply.github.com>
2021-10-10 16:46:29 +0200

    Update promocionCDEnNuevoBosque.ps1

--- promocionCDEnNuevoBosque.ps1
+++ promocionCDEnNuevoBosque.ps1
@@ -1,15 +1,14 @@
-#Referencia
-# https://docs.microsoft.com/es-es/windows-server/identity/ad-ds/deploy/install-active-directory-domain-services--level-100-
-
-#
-# Script de Windows PowerShell para implementación de AD DS
-#
-# cmdlet para instalar el rol AD DS y las herramientas de administración de Active Directory.
-Install-WindowsFeature -name AD-Domain-Services -IncludeManagementTools 
-
-# cmdlet de promoción a CD en un nuevo bosque
-if (!(Get-Module -Name ADDSDeployment)) #Se comprueba si se tiene cargado el módulo
-{
-  Import-Module ADDSDeployment #Se carga el módulo
-}
-Install-ADDSForest -CreateDnsDelegation:$false -DatabasePath "C:\Windows\NTDS" -DomainMode "Default" -DomainName "smr.local" -DomainNetbiosName "SMR" -ForestMode "Default" -InstallDns:$true -LogPath "C:\Windows\NTDS" -SysvolPath "C:\Windows\SYSVOL" -Force:$true
+PS C:\> Install-ADDSDomainController `
+-DomainName "upv.es" `
+-Credential (Get-Credential) `
+-SiteName "Default-First-Site-Name" `
+-InstallDNS:$true `
+-NoGlobalCatalog:$false `
+-CreateDNSDelegation:$false `
+-ReplicationSourceDC "master-upv-es-r.upv.es" `
+-CriticalReplicationOnly:$false `
+-DatabasePath "C:\Windows\NTDS" `
+-LogPath "C:\Windows\NTDS" `
+-NoRebootOnCompletion:$false `
+-SysvolPath "C:\Windows\SYSVOL" `
+-Force:$true
